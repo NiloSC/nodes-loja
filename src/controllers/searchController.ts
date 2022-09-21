f34c1d2a6a9cd33e24084c541694924650e94f01
NiloSC <nilo.sc@hotmail.com>
2022-09-20 21:56:41 -0300

    projeto finalizado

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -4,12 +4,22 @@ import { createMenuObject } from "../helpers/createMenuObject.";
 
 export const search = (req: Request, res: Response) => {
     let query: string = req.query.q as string;
+    if (!query){
+        res.redirect('/');
+        return;
+    }
+
+
+
     let list = Clothe.getFromName(query);
 
 res.render('pages/page' , {
     menu: createMenuObject(''),
     list,
+    query,
 });
 };
+
+
 
 
